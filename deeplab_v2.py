24448b8455b1c7ab88adc1d3b785ab7cb3da304d
bhappy10 <skypia0906@gmail.com>
2018-08-29 18:53:00 +0900

    code fix

--- deeplab_v2.py
+++ deeplab_v2.py
@@ -1,164 +1,200 @@
 import tensorflow as tf
 import numpy as np
-import random
-import os
+import os, random
+import tensorflow.contrib.slim as slim
 
 from tqdm import tqdm
-from custom_op import conv2d, conv2d_t, relu, bn, max_pool, atrous_conv2d
-from utils import read_image, load_data_path, next_batch
-
-
-def identity_block(inputs, filters, stage, is_training=True):
-    filter1, filter2, filter3 = filters
-    layer1 = relu(bn(conv2d(inputs, filter1, 1, 1, name=stage+'_a_identity', padding='VALID'), is_training))
-    layer2 = relu(bn(conv2d(layer1, filter2, 3, 3, name=stage+'_b_identity'), is_training))
-    layer3 = bn(conv2d(layer2, filter3, 1, 1, name=stage+'_c_identity', padding='VALID'), is_training)
-    layer4 = relu(tf.add(layer3, inputs))
-
-    return layer4
-
-def conv_block(inputs, filters, stage, s=2, is_training=True):
-    filter1, filter2, filter3 = filters
-    layer1 = relu(bn(conv2d(inputs, filter1, 1, 1, name=stage+'_a_conv', strides=[1, s, s, 1], padding='VALID'), is_training))
-    layer2 = relu(bn(conv2d(layer1, filter2, 3, 3, name=stage+'_b_conv'), is_training))
-    layer3 = bn(conv2d(layer2, filter3, 1, 1, name=stage+'_c_conv', padding='VALID'), is_training)
-    shortcut = bn(conv2d(inputs, filter3, 1, 1, name=stage+'_shortcut', strides=[1, s, s, 1], padding='VALID'), is_training)
-    layer4 = relu(tf.add(layer3, shortcut))
-    
-    return layer4
-
-def atrous_identity_block(inputs, filters, stage, rate, is_training=True):
-    filter1, filter2, filter3 = filters
-    layer1 = relu(bn(atrous_conv2d(inputs, filter1, 1, 1, rate, name=stage+'_a_identity'), is_training))
-    layer2 = relu(bn(atrous_conv2d(layer1, filter2, 3, 3, rate, name=stage+'_b_identity'), is_training))
-    layer3 = bn(atrous_conv2d(layer2, filter3, 1, 1, rate, name=stage+'_c_identity'), is_training)
-    layer4 = relu(tf.add(layer3, inputs))
-
-    return layer4
-
-def atrous_conv_block(inputs, filters, stage, rate, s=2, is_training=True):
-    filter1, filter2, filter3 = filters
-    layer1 = relu(bn(atrous_conv2d(inputs, filter1, 1, 1, rate, name=stage+'_a_conv'), is_training))
-    layer2 = relu(bn(atrous_conv2d(layer1, filter2, 3, 3, rate, name=stage+'_b_conv'), is_training))
-    layer3 = bn(atrous_conv2d(layer2, filter3, 1, 1, rate, name=stage+'_c_conv'), is_training)
-    shortcut = bn(conv2d(inputs, filter3, 1, 1, name=stage+'_shortcut', strides=[1, s, s, 1], padding='VALID'), is_training)
-    layer4 = relu(tf.add(layer3, shortcut))
-    
-    return layer4
+from custom_op import conv2d, conv2d_t, atrous_conv2d, max_pool, lrelu, bn, relu
+from utils import read_data_path, next_batch, read_image, read_annotation, draw_plot
 
 
 class DeepLab_v2(object):
-    def __init__(self):
-        self.N_BATCH = 2
-        self.N_EPOCH = 1000
-        self.N_CLASS = 151
-        self.L_RATE = 1e-5
+    MODEL = 'DeepLab_v2'
 
-        self.IMAGE_DATA_DIR = '../dataset/images/'
-        self.ANNOTATION_DATA_DIR = '../dataset/annotations/'
-        self.LOG_DIR = './logs/DeepLab_v2/'
+    def __init__(self, epoch, batch, learning_rate):
+        self.N_EPOCH = epoch
+        self.N_BATCH = batch
+        self.LEARNING_RATE = learning_rate
+
         self.MODEL_NAME = 'DeepLab_v2'
 
+        self.LOGS_DIR = os.path.join(self.MODEL_NAME+'_result', 'logs')
+        self.CKPT_DIR = os.path.join(self.MODEL_NAME+'_result', 'ckpt')
+        self.OUTPUT_DIR = os.path.join(self.MODEL_NAME+'_result', 'output')
+        
+        self.N_CLASS = 151
+        self.RESIZE = 192
+        
+        self.TRAIN_IMAGE_PATH = './DATA/ADEChallengeData2016/images/training/'
+        self.TRAIN_LABEL_PATH = './DATA/ADEChallengeData2016/annotations/training/'
+
+        self.VALID_IMAGE_PATH = './DATA/ADEChallengeData2016/images/validation/'
+        self.VALID_LABEL_PATH = './DATA/ADEChallengeData2016/annotations/validation/'
+
     
-    def model(self, inputs, is_training=True):
+    def make_model(self, inputs, is_training):
         """
             extract feature using ResNet. Encoder
 
         """
         with tf.variable_scope('ResNet50'):
-            x = conv2d(inputs, 64, 7, 7, name='conv1', strides=[1, 2, 2, 1])    # size 1/2
-            x = bn(x, is_training=True)
+            x = conv2d(inputs, 64, [7, 7], strides=[1, 2, 2, 1], name='conv1')    # size 1/2
+            x = bn(x, is_training)
             x = relu(x)
-            x = max_pool(x, 'pool1', ksize=[1, 3, 3, 1])                        # size 1/4
-            print('1: ', x)
+            x = max_pool(x, ksize=[1, 3, 3, 1], name='pool1')                        # size 1/4
 
-            x = conv_block(x, [64, 64, 256], '2_1', s=1)
-            x = identity_block(x, [64, 64, 256], '2_2')
-            x = identity_block(x, [64, 64, 256], '2_3')
-            print('2: ', x)
+            x = self.conv_block(x, [64, 64, 256], '2_1', is_training, s=1)
+            x = self.identity_block(x, [64, 64, 256], '2_2', is_training)
+            x = self.identity_block(x, [64, 64, 256], '2_3', is_training)
 
-            x = conv_block(x, [128, 128, 512], '3_1')
-            x = identity_block(x, [128, 128, 512], '3_2')
-            x = identity_block(x, [128, 128, 512], '3_3')
-            print('3: ', x)
+            x = self.conv_block(x, [128, 128, 512], '3_1', is_training)
+            x = self.identity_block(x, [128, 128, 512], '3_2', is_training)
+            x = self.identity_block(x, [128, 128, 512], '3_3', is_training)
 
-            x = atrous_conv_block(x, [256, 256, 1024], '4_1', rate=2, s=1)
-            x = atrous_identity_block(x, [256, 256, 1024], '4_2', rate=2)
-            x = atrous_identity_block(x, [256, 256, 1024], '4_3', rate=2)
-            x = atrous_identity_block(x, [256, 256, 1024], '4_4', rate=2)
-            x = atrous_identity_block(x, [256, 256, 1024], '4_5', rate=2)
-            x = atrous_identity_block(x, [256, 256, 1024], '4_6', rate=2)
-            print('4: ', x)
+            x = self.atrous_conv_block(x, [256, 256, 1024], '4_1', 2, is_training, s=1)
+            x = self.atrous_identity_block(x, [256, 256, 1024], '4_2', 2, is_training)
+            x = self.atrous_identity_block(x, [256, 256, 1024], '4_3', 2, is_training)
+            x = self.atrous_identity_block(x, [256, 256, 1024], '4_4', 2, is_training)
+            x = self.atrous_identity_block(x, [256, 256, 1024], '4_5', 2, is_training)
+            x = self.atrous_identity_block(x, [256, 256, 1024], '4_6', 2, is_training)
 
-            x = atrous_conv_block(x, [512, 512, 2048], '5_1', rate=4, s=1)
-            x = atrous_identity_block(x, [512, 512, 2048], '5_2', rate=4)
-            x = atrous_identity_block(x, [512, 512, 2048], '5_3', rate=4)
-            print('5: ', x)
-
+            x = self.atrous_conv_block(x, [512, 512, 2048], '5_1', 4, is_training, s=1)
+            x = self.atrous_identity_block(x, [512, 512, 2048], '5_2', 4, is_training)
+            x = self.atrous_identity_block(x, [512, 512, 2048], '5_3', 4, is_training)
 
         """
             Astrous Pyrimid Pooling. Decoder
         """
         with tf.variable_scope('ASPP'):
-            rate6 = atrous_conv2d(x, self.N_CLASS, 3, 3, 6, name='rate6')
-            rate6 = conv2d(rate6, self.N_CLASS, 1, 1, name='rate6_conv1')
-            rate6 = conv2d(rate6, self.N_CLASS, 1, 1, name='rate6_conv2')
+            rate6 = atrous_conv2d(x, self.N_CLASS, [3, 3], 6, name='rate6')
+            rate6 = conv2d(rate6, self.N_CLASS, [1, 1], name='rate6_conv1')
+            rate6 = conv2d(rate6, self.N_CLASS, [1, 1], name='rate6_conv2')
 
-            rate12 = atrous_conv2d(x, self.N_CLASS, 3, 3, 12, name='rate12')
-            rate12 = conv2d(rate12, self.N_CLASS, 1, 1, name='rate12_conv1')
-            rate12 = conv2d(rate12, self.N_CLASS, 1, 1, name='rate12_conv2')
+            rate12 = atrous_conv2d(x, self.N_CLASS, [3, 3], 12, name='rate12')
+            rate12 = conv2d(rate12, self.N_CLASS, [1, 1], name='rate12_conv1')
+            rate12 = conv2d(rate12, self.N_CLASS, [1, 1], name='rate12_conv2')
 
-            rate18 = atrous_conv2d(x, self.N_CLASS, 3, 3, 18, name='rate18')
-            rate18 = conv2d(rate18, self.N_CLASS, 1, 1, name='rate18_conv1')
-            rate18 = conv2d(rate18, self.N_CLASS, 1, 1, name='rate18_conv2')
+            rate18 = atrous_conv2d(x, self.N_CLASS, [3, 3], 18, name='rate18')
+            rate18 = conv2d(rate18, self.N_CLASS, [1, 1], name='rate18_conv1')
+            rate18 = conv2d(rate18, self.N_CLASS, [1, 1], name='rate18_conv2')
             
-            rate24 = atrous_conv2d(x, self.N_CLASS, 3, 3, 24, name='rate24')
-            rate24 = conv2d(rate24, self.N_CLASS, 1, 1, name='rate24_conv1')
-            rate24 = conv2d(rate24, self.N_CLASS, 1, 1, name='rate24_conv2')
+            rate24 = atrous_conv2d(x, self.N_CLASS, [3, 3], 24, name='rate24')
+            rate24 = conv2d(rate24, self.N_CLASS, [1, 1], name='rate24_conv1')
+            rate24 = conv2d(rate24, self.N_CLASS, [1, 1], name='rate24_conv2')
 
-            self.logits = tf.add_n([rate6, rate12, rate18, rate24])
-            self.out = tf.image.resize_bilinear(self.logits, size=[192, 192])
-            print(self.logits)
-            print(self.out)
+            # self.logits = tf.add_n([rate6, rate12, rate18, rate24])
+            # self.out = tf.image.resize_bilinear(self.logits, size=[192, 192])
+            add_aspp = tf.add_n([rate6, rate12, rate18, rate24])
+            pred = tf.image.resize_bilinear(add_aspp, size=[self.RESIZE, self.RESIZE])
 
-            return self.logits, self.out
+            return pred
 
     def build_model(self):
-        self.INPUT_X = tf.placeholder(dtype=tf.float32, shape=[None, 192, 192, 3])
-        self.INPUT_Y = tf.placeholder(dtype=tf.int32, shape=[None, 192, 192, 1])
+        self.input_x = tf.placeholder(dtype=tf.float32, shape=[None, self.RESIZE, self.RESIZE, 3])         # images
+        self.label_y = tf.placeholder(dtype=tf.int32, shape=[None, self.RESIZE, self.RESIZE, 1])         # annotations
+        self.is_train = tf.placeholder(dtype=tf.bool)
 
-        self.logits, self.out = self.model(self.INPUT_X)
-
-        self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.out, labels=tf.squeeze(self.INPUT_Y, [3])))
-        self.optimizer = tf.train.AdamOptimizer(self.L_RATE).minimize(self.loss)
+        self.pred = self.make_model(self.input_x, self.is_train)
+        
+        self.loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.pred, labels=tf.squeeze(self.label_y, [3])))
+        
+        self.optimizer = tf.train.AdamOptimizer(self.LEARNING_RATE).minimize(self.loss)
 
         self.loss_summary = tf.summary.merge([tf.summary.scalar('loss', self.loss)])
+    
+        model_vars = tf.trainable_variables()
+        slim.model_analyzer.analyze_vars(model_vars, print_info=True)
         
 
     
     def train_model(self):
-        data_set_path = load_data_path(self.IMAGE_DATA_DIR, self.ANNOTATION_DATA_DIR, 'training')
-        valid_set_path = load_data_path(self.IMAGE_DATA_DIR, self.ANNOTATION_DATA_DIR, 'validation')
+        if not os.path.exists(self.MODEL_NAME+'_result'):   os.mkdir(self.MODEL_NAME+'_result')
+        if not os.path.exists(self.LOGS_DIR):   os.path.exists(self.LOGS_DIR)
+        if not os.path.exists(self.CKPT_DIR):   os.path.exists(self.CKPT_DIR)
+        if not os.path.exists(self.OUTPUT_DIR): os.path.exists(self.OUTPUT_DIR)
+        
+        train_set_path = read_data_path(self.TRAIN_IMAGE_PATH, self.TRAIN_LABEL_PATH)
+        valid_set_path = read_data_path(self.VALID_IMAGE_PATH, self.VALID_LABEL_PATH)
         
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
+            total_batch = int(len(train_set_path) / self.N_BATCH)
+            counter = 0
+
+            self.saver = tf.train.Saver()
+            self.writer = tf.summary.FileWriter(self.LOGS_DIR, sess.graph)
+
             for epoch in range(self.N_EPOCH):
-                random.shuffle(data_set_path)           # 매 epoch마다 데이터셋 shuffling
+                total_loss = 0
+                random.shuffle(train_set_path)           # 매 epoch마다 데이터셋 shuffling
+                random.shuffle(valid_set_path)
 
-                for i in range(int(len(data_set_path) / 2)):
-                    batch_img_path, batch_ann_path = next_batch(data_set_path, i, self.N_BATCH)
-                    batch_imgs, batch_anns = read_image(batch_img_path, batch_ann_path, self.N_BATCH, 192, 192)
+                for i in range(int(len(train_set_path) / self.N_BATCH)):
+                    batch_xs_path, batch_ys_path = next_batch(train_set_path, self.N_BATCH, i)
+                    batch_xs = read_image(batch_xs_path, [self.RESIZE, self.RESIZE])
+                    batch_ys = read_annotation(batch_ys_path, [self.RESIZE, self.RESIZE])
 
-                    _ ,loss_val = sess.run([self.optimizer, self.loss], feed_dict={self.INPUT_X:batch_imgs, self.INPUT_Y:batch_anns})
-                    # self.writer.add_summary(summary_str, counter)
-                    # counter += 1
+                    feed_dict = {self.input_x: batch_xs, self.label_y: batch_ys, self.is_train: True}
 
-                print('EPOCH: {}\t'.format(epoch+1), 'LOSS: {:.8}\t'.format(loss_val))
+                    _, summary_str ,loss = sess.run([self.optimizer, self.loss_summary, self.loss], feed_dict=feed_dict)
+                    self.writer.add_summary(summary_str, counter)
+                    counter += 1
+                    total_loss += loss
+
+                ## validation 과정
+                valid_xs_path, valid_ys_path = next_batch(valid_set_path, epoch, 2)
+                valid_xs, valid_ys = read_image(valid_xs_path, valid_ys_path, 2)
+                
+                valid_pred = sess.run(self.pred, feed_dict={self.input_x: valid_xs, self.label_y: valid_ys, self.is_train:False})
+                valid_pred = np.squeeze(valid_pred, axis=2)
+                
+                valid_ys = np.squeeze(valid_ys, axis=3)
+
+                ## plotting and save figure
+                figure = draw_plot(valid_xs, valid_pred, valid_ys, self.OUTPUT_DIR, epoch, self.batch)
+                figure.savefig(self.OUTPUT_DIR + '/' + str(epoch).zfill(3) + '.png')
+
+                print('Epoch:', '%03d' % (epoch + 1), 'Avg Loss: {:.6}\t'.format(total_loss / total_batch))
+                self.saver.save(self.CKPT_DIR, global_step=counter)
+
+            self.saver.save(self.CKPT_DIR, global_step=counter)
+
+
+    def identity_block(self, inputs, filters, stage, is_training):
+        filter1, filter2, filter3 = filters
+        layer1 = relu(bn(conv2d(inputs, filter1, [1, 1], name=stage+'_a_identity', padding='VALID'), is_training))
+        layer2 = relu(bn(conv2d(layer1, filter2, [3, 3], name=stage+'_b_identity'), is_training))
+        layer3 = bn(conv2d(layer2, filter3, [1, 1], name=stage+'_c_identity', padding='VALID'), is_training)
+        layer4 = relu(tf.add(layer3, inputs))
+        return layer4
+
+
+    def conv_block(self, inputs, depths, stage, is_training, s=2):
+        depth1, depth2, depth3 = depths
+        layer1 = relu(bn(conv2d(inputs, depth1, [1, 1], name=stage+'_a_conv', strides=[1, s, s, 1], padding='VALID'), is_training))
+        layer2 = relu(bn(conv2d(layer1, depth2, [3, 3], name=stage+'_b_conv'), is_training))
+        layer3 = bn(conv2d(layer2, depth3, [1, 1], name=stage+'_c_conv', padding='VALID'), is_training)
+        shortcut = bn(conv2d(inputs, depth3, [1, 1], name=stage+'_shortcut', strides=[1, s, s, 1], padding='VALID'), is_training)
+        layer4 = relu(tf.add(layer3, shortcut))
+        return layer4
+
         
+    def atrous_identity_block(self, inputs, depths, stage, rate, is_training):
+        depth1, depth2, depth3 = depths
+        layer1 = relu(bn(atrous_conv2d(inputs, depth1, [1, 1], rate, name=stage+'_a_identity'), is_training))
+        layer2 = relu(bn(atrous_conv2d(layer1, depth2, [3, 3], rate, name=stage+'_b_identity'), is_training))
+        layer3 = bn(atrous_conv2d(layer2, depth3, [1, 1], rate, name=stage+'_c_identity'), is_training)
+        layer4 = relu(tf.add(layer3, inputs))
+        return layer4
 
 
-
-model = DeepLab_v2()
-model.build_model()
-model.train_model()
+    def atrous_conv_block(self, inputs, depths, stage, rate, is_training, s=2):
+        depth1, depth2, depth3 = depths
+        layer1 = relu(bn(atrous_conv2d(inputs, depth1, [1, 1], rate, name=stage+'_a_conv'), is_training))
+        layer2 = relu(bn(atrous_conv2d(layer1, depth2, [3, 3], rate, name=stage+'_b_conv'), is_training))
+        layer3 = bn(atrous_conv2d(layer2, depth3, [1, 1], rate, name=stage+'_c_conv'), is_training)
+        shortcut = bn(conv2d(inputs, depth3, [1, 1], name=stage+'_shortcut', strides=[1, s, s, 1], padding='VALID'), is_training)
+        layer4 = relu(tf.add(layer3, shortcut))
+        return layer4
